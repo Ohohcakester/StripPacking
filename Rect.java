a89f281f5790b181fd8c7a287407949c17ae2914
Ohohcakester <ohohgit@gmail.com>
2015-10-31 22:33:10 +0800

    Drawing code for rectangles

--- Rect.java
+++ Rect.java
@@ -5,10 +5,15 @@ public class Rect {
 	public float y2;
 	
 	public Rect(float x1, float y1, float x2, float y2) {
+		if (x2<x1 || y2<y1) throw new UnsupportedOperationException("INVALID RECTANGLE");
 		this.x1 = x1;
 		this.y1 = y1;
 		this.x2 = x2;
 		this.y2 = y2;
+	}
+
+	public static Rect place(float x, float y, float width, float height) {
+		return new Rect(x,y,x+width,y+height);
 	}
 	
 	public boolean overlaps(Rect other) {
--- StripPackingUI.java
+++ StripPackingUI.java
@@ -4,12 +4,17 @@ import javafx.scene.Scene;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
+import java.util.*;
 
 public class StripPackingUI extends Application {
 
     public static Group root;
-    public static final int resX = 300;
-    public static final int resY = 250;
+    public static final int resX = 800;
+    public static final int resY = 600;
+    public static int realHeight = 800;
+    public static int realWidth = 600;
+
+    private ArrayList<Rectangle> existingRectangles = new ArrayList<>();
 
     public static void main(String[] args) {
         Application.launch(args);
@@ -21,15 +26,51 @@ public class StripPackingUI extends Application {
         root = new Group();
         Scene scene = new Scene(root, resX, resY, Color.WHITE);
         
-        drawRectangle(0,0,200,100);
+        Rect[] rects = new Rect[]{
+            new Rect(0,0,200,200),
+            new Rect(200,0,250,50),
+            new Rect(250,0,800,600)
+        };
+        redraw(Arrays.asList(rects), 800, 800);
 
         primaryStage.setScene(scene);
         primaryStage.show();
     }
 
-    public void drawRectangle(int x1,int y1, int x2, int y2) {
+    public void redraw(Rect[] rects, int width, int height) {
+        redraw(Arrays.asList(rects), width, height);
+    }
+
+    public void redraw(Iterable<Rect> rects, int width, int height) {
+        clearRectangles();
+        float scale = Math.min((float)resX/width, (float)resY/height);
+
+        drawRectangle(width*scale,0,resX,resY,1, Color.BLACK);
+        drawRectangle(0,height*scale,resX,resY,1, Color.BLACK);
+
+        for (Rect rect : rects) {
+            drawRectangle(rect, scale);
+        }
+    }
+
+    private void clearRectangles() {
+        for (Rectangle r : existingRectangles) {
+            root.getChildren().remove(r);
+        }
+        existingRectangles.clear();
+    }
+
+    private void addRectangle(Rectangle r) {
+        existingRectangles.add(r);
+        root.getChildren().add(r);
+    }
+
+    public void drawRectangle(float x1,float y1, float x2, float y2, float scale, Color color) {
         Rectangle r = new Rectangle();
-        int temp = y2;
+        r.setFill(color);
+        x1 *= scale; x2 *= scale; y1 *= scale; y2 *= scale;
+
+        float temp = y2;
         y2 = resY-y1;
         y1 = resY-temp;
 
@@ -38,6 +79,10 @@ public class StripPackingUI extends Application {
         r.setWidth(x2-x1);
         r.setHeight(y2-y1);
         
-        root.getChildren().add(r); 
+        addRectangle(r); 
+    }
+
+    public void drawRectangle(Rect rect, float scale) {
+        drawRectangle(rect.x1,rect.y1,rect.x2,rect.y2, scale, Color.GREEN);
     }
 }
