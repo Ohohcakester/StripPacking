7bf6c9dccb5cd96fa90f0f06e53254d634fad1de
Ohohcakester <ohohgit@gmail.com>
2015-11-06 07:31:05 +0800

    Update CaseGen.java for more flexibility

--- CaseGen.java
+++ CaseGen.java
@@ -2,22 +2,36 @@ import java.io.FileWriter;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.Random;
 
 public class CaseGen {
+    Random random = new Random();
+
 	public static void main(String[] args) throws IOException {
 		BufferedWriter fw = new BufferedWriter(new FileWriter("test.txt"));
 		Scanner sc  = new Scanner(System.in);
+        System.out.println("Input number of items");
 		int n = sc.nextInt();
+        System.out.println("Input strip width");
 		int scale = sc.nextInt();
 		fw.write(n + " " + scale);
 		fw.newLine();
 		for (int i = 0; i < n; i++) {
-			int width = (int) Math.floor((Math.random() * scale) + 1);
-			int height = (int) Math.floor((Math.random() * scale) + 1);
+			int width = (int) Math.floor(widthFun(scale));
+			int height = (int) Math.floor(heightFun(scale));
+            width = Math.min(width, scale);
 			fw.write(width + " " + height);
 			fw.newLine();
 		}
 		fw.flush();
 		fw.close();
 	}
+    
+    public static double widthFun(int scale) {
+        return (Math.random()*(Math.random()+0.15) * scale) + 1;
+    }
+    
+    public static double heightFun(int scale) {
+        return (Math.random()*(Math.random()+0.3) * scale) + 1;
+    }
 }
